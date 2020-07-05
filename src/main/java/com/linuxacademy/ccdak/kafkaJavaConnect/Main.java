bb76e4858a6bc31119e77eb28c12adef7439a9a4
kainvey@gmail.com <kainvey@gmail.com>
2020-07-05 17:06:19 +1200

    update

--- src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
+++ src/main/java/com/linuxacademy/ccdak/kafkaJavaConnect/Main.java
@@ -1,8 +1,23 @@
 package com.linuxacademy.ccdak.kafkaJavaConnect;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = new KafkaProducer<String, String>(props);
+        for(int i=0; i<100; i++){
+            producer.send(new ProducerRecord<String, String>("count-topic", "count", Integer.toString(i)));
+        }
+        producer.close();
+
     }
 }
